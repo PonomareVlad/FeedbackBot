1e213339a293853e2c7f39e1a874b829d8fb4418
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2025-05-05 03:31:42 +0200

    Add new dependencies and implement bot enhancements
    Added `@grammyjs/stateless-question`, `@roziscoding/grammy-autoquote`, and `prettier` to the project dependencies. Enhanced the bot by introducing stateless question handling, automatic quoting of messages, and interactive inline keyboard features for better user interaction. Adjusted relevant commands and middleware to integrate these improvements.

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,9 @@
-import { Bot } from 'grammy'
+import { Bot, InlineKeyboard } from 'grammy'
+import { autoQuote } from '@roziscoding/grammy-autoquote'
+import { StatelessQuestion } from '@grammyjs/stateless-question'
 
 export const {
-    TELEGRAM_CHAT_ID: id,
+    TELEGRAM_CHAT_ID: chat,
     TELEGRAM_BOT_TOKEN: token,
     TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(':').pop(),
 } = process.env
@@ -10,6 +12,33 @@ export const bot = new Bot(token)
 
 const safe = bot.errorBoundary(console.error)
 
-safe.command('start', ctx => ctx.reply('Вы можете отправить произвольное количество сообщений с любым содержанием'))
+const answerQuestion = new StatelessQuestion('answer', (ctx, id) =>
+    ctx.copyMessage(id).then(() => ctx.react('👌'))
+)
 
-safe.on('msg', ctx => ctx.forwardMessage(id).then(() => ctx.react('👌')))
+safe.use(autoQuote())
+
+safe.use(answerQuestion.middleware())
+
+safe.command('start', ctx =>
+    ctx.reply(`
+Вы можете отправить произвольное количество сообщений с любым содержанием
+`)
+)
+
+safe.on('msg', ctx =>
+    ctx
+        .copyMessage(chat, {
+            reply_markup: new InlineKeyboard().text(
+                'Ответить',
+                String(ctx.chatId)
+            ),
+        })
+        .then(() => ctx.react('👌'))
+)
+
+safe.on('callback_query:data', ctx =>
+    answerQuestion
+        .replyWithMarkdown(ctx, 'Напишите ваш ответ', ctx.callbackQuery.data)
+        .finally(() => ctx.answerCallbackQuery())
+)
